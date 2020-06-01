8390aac8f8e139d01a672cb4602be75040583fb7
fabr1991 <56141382+fabr1991@users.noreply.github.com>
2020-05-31 20:36:58 -0400

    cambio

--- autofarma/settings.py
+++ autofarma/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'spgp9n@+!5$+no63hrfxkw2))t$xlgrgybm)rwo^qe45vv#&yw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.104']
 
 
 # Application definition
